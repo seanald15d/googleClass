e0895b43f9ead69b3159a25f86e1aec11fe2dd36
seanald15d <s.mccullough@tcu.edu>
2020-02-06 10:31:00 -0600

    Added files now!

--- connectGoogle.py
+++ connectGoogle.py
@@ -15,6 +15,50 @@ def read_class(file_string):
     return c_list
 
 
+def create_remote_document(drive_service, document_name, folder_id, parent_id=None):
+    results = drive_service.files().list(q="mimeType='application/vnd.google-apps.folder' and name='" + parent_id + "'",
+                                         pageSize=10).execute()
+    items = results.get("files", [])
+
+    print(items)
+
+    parents = []
+    final_loc = []
+    for item in items:
+        parents.append(drive_service.files().get(fileId=item['id'],
+                                                 fields='parents').execute())
+        for parent in parents:
+            potential = (drive_service.files().get(fileId=parent["parents"][0]).execute())
+            if potential["name"] == "Project 2":
+                final_loc.append(item)
+
+    print(final_loc)
+
+    if items[1]['name'] == parent_id:
+        parent_folder = items[1]
+
+    # update document on drive
+    file_id = '1Y2ChcMRcNjCOysiRkTmdCS8TmVUtcd1ZpQ9lNj849Eo'
+    file = drive_service.files().update(fileId=file_id,
+                                        addParents=final_loc[0]['id'],
+                                        fields='id, parents').execute()
+    '''
+    title = document_name
+    print(title)
+    body = {
+        "title": title,
+        'mimeType': "application/vnd.google-apps.document"
+    }
+
+    if parent_id is not None:
+        try:
+            body['parents'] = [final_loc[0]['id']]
+        except UnboundLocalError:
+            print("moving on")
+    '''
+    # drive_service.files().create(body=body).execute()
+
+
 def create_remote_folder(drive_service, folder_name, folder_items, parent_id=None):
     # find folder by parent name
     for item in folder_items:
@@ -27,7 +71,23 @@ def create_remote_folder(drive_service, folder_name, folder_items, parent_id=Non
     }
     if parent_id:
         body['parents'] = [parent_folder['id']]
-    drive_service.files().create(body=body).execute()
+
+    # before execute, need to check that folder returns what we want
+    # drive_service.files().create(body=body).execute()
+
+
+def make_docs(cl):
+    for name in cl:
+        create_remote_document(drive_service=s, document_name=f"{name}_project2_brainstorming",
+                               folder_id=i, parent_id=name)
+
+
+def make_folders(cl):
+    for name in cl:
+        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 1")
+        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 2")
+        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 3")
+        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 4")
 
 
 def main():
@@ -56,24 +116,24 @@ def main():
     service = build('drive', 'v3', credentials=creds)
 
     # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    results = service.files().list(pageSize=1000, fields='nextPageToken, files(id, name)').execute()
+    desired_folder = "Project 2"
+
     items = results.get('files', [])
 
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
+    wanted_list = []
 
-    return service, items
+    for item in items:
+        if item['name'] == desired_folder:
+            wanted_list.append(item)
+
+    return service, wanted_list
 
 
 if __name__ == '__main__':
     s, i = main()
 
     class_list = read_class(file_string="class.txt")
-    for name in class_list:
-        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 1")
-        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 2")
-        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 3")
-        create_remote_folder(drive_service=s, folder_name=name, folder_items=i, parent_id="Project 4")
+
+    # make_folders(class_list)
+    make_docs(class_list)
